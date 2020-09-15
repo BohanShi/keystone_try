0e71c5bef43be30d600c6c6d91efff85a8c317da
Raguram <rdraguram@gmail.com>
2020-09-15 16:31:17 +1000

    Worked on Open data fragment Extracted data from DB 15/09

--- app/src/main/java/com/example/keystone_try/ui/OpenData/OpenData.java
+++ app/src/main/java/com/example/keystone_try/ui/OpenData/OpenData.java
@@ -3,14 +3,15 @@ package com.example.keystone_try.ui.OpenData;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.keystone_try.R;
@@ -20,41 +21,79 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Objects;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import static android.content.ContentValues.TAG;
+import java.util.ArrayList;
+import java.util.List;
 
+import static android.text.method.LinkMovementMethod.getInstance;
 
 public class OpenData extends Fragment {
 
-DatabaseReference ref;
-TextView a;
+    //insert codes for data extraction
 
+    DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Census");
+    List<String> census = new ArrayList<String>();
+//    List<Object> age_group = new
+//    class abc {
+//        private String ageGroup;
+//        public abc(String ageGroup){
+//            this.ageGroup = ageGroup;
+//        }
+//    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        setupHyperlink(view);
+    }
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
 
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_open_data, container, false);
 
-        //insert codes for data extraction
-        //a = (TextView)findViewById(R.id.test_text);
-
-        ref = FirebaseDatabase.getInstance().getReference().child("Census").child("1");
-
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String age = Objects.requireNonNull(snapshot.child("Age Group").getValue()).toString();
-                a.setText(age);
-            }
 
+                //List<String> keys = new ArrayList<>();
+//                for (DataSnapshot keyNode : snapshot.getChildren()) {
+//                    //keys.add(keyNode.getKey());
+//                    //Log.d(String.valueOf(keyNode.getValue()),"None");
+//                    String census_new = String.valueOf(keyNode.getValue());
+//                    census.add(census_new);
+//                }
+                //Log.d(String.valueOf(census), "Output_test");
+                for (DataSnapshot keyNode : snapshot.getChildren())
+                {
+                    String ageGroup = (String) keyNode.child("Age_Group").getValue();
+
+
+                   Log.i("Value of element ",ageGroup);
+
+                }
+
+            }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
             }
         });
+        //ref= null;
         return root;
     }
+
+    private void setupHyperlink(View v)  {
+
+        TextView link = v.findViewById(R.id.OD_Content5);
+       //link.setText(Html.fromHtml(getString(R.string.OD_link01)));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
+
+
+    }
+
+
 }
